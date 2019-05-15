f4a817a896680f0c8926a1981ace7268ca1ab2bd
Michal Bojanowski <michal2992@gmail.com>
2019-05-15 02:12:45 +0200

    Export plot_data_model()

--- R/data_model.R
+++ R/data_model.R
@@ -1,25 +1,30 @@
-
-data_model <- function() {
-  requireNamespace("datamodelr")
-  dm <- dm_from_data_frames(reconqdata::collaboration, reconqdata::nodes, reconqdata::resources)
-  dm_add_references(
-    dm,
-    resources$id_interview == nodes$id_interview,
-    resources$from == nodes$id_node,
-    resources$to == nodes$id_node,
-    collaboration$id_interview == nodes$id_interview,
-    collaboration$from == nodes$id_node,
-    collaboration$to == nodes$id_node
-  )
-}
-
+#' Plot a data model
+#'
+#' @export
 plot_data_model <- function() {
   requireNamespace("datamodelr")
-  dm_create_graph(
+  datamodelr::dm_create_graph(
     data_model(),
     rankdir="BT",
     col_attr = c("column", "type"),
-    columnArrows = TRUE
+    columnArrows = FALSE
+  )
+}
+
+
+
+
+data_model <- function() {
+  requireNamespace("datamodelr")
+  dm <- datamodelr::dm_from_data_frames(collaboration, nodes, resources)
+  datamodelr::dm_add_references(
+    dm,
+    resources$id_interview == nodes$id_interview,
+    collaboration$id_interview == nodes$id_interview,
+    resources$from == nodes$id_node,
+    resources$to == nodes$id_node,
+    collaboration$from == nodes$id_node,
+    collaboration$to == nodes$id_node
   )
 }
 
